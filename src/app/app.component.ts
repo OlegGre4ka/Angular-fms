245b40aa43bd59f111ad365c905125856821e79c
OlegGre4ka <gre4kae@ukr.net>
2018-02-11 12:29:01 +0300

    V4.5-[ngSwitch]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   headerText = 'Створення власної директиви';
+  items = [1, 2, 3, 4, 5];
+  current = 0;
+  onClick(number: number) {
+this.current = number;
+  }
 }
 
 
